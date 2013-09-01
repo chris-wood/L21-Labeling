bc210c89bdfe8ac70141b3fb43754332a9590805
Christopher Wood <cwood@capri-wlan.parc.xerox.com>
2013-09-01 16:08:10 -0700

    Wrote the partite bagger - need to run it now

--- src/bicubic/partite_bagger.py
+++ src/bicubic/partite_bagger.py
@@ -5,10 +5,22 @@ import sys
 fname = sys.argv[1]
 graphs = build_graph_from_asc_file(fname)
 
+partite_bags = {}
+
 index = 0
 for G in graphs:
+	# Save a picture of the graph so this can be checked!
 	P = G.plot()
 	P.save(prefix + "_" + str(index) + ".png")
-	print("Graph: " + prefix + "_" + str(index) + ".png")
-	print(G.adjacency_matrix().str())
-	print(G.chromatic_number())
+	t = G.chromatic_number()
+
+	# Update the map
+	if not (t in partite_bags):
+		partite_bags[t] = []
+	partite_bags[t].append((G, prefix + "_" + str(index) + ".png"))
+
+# Display the graphs in each bag
+for t in partite_bags:
+	print("chi: " + str(t))
+	for (G, fn) in partite_bags[t]:
+		print(fn)
