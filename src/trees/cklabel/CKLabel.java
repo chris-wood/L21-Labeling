fb6361733d397007202d1039e041c30078632dc5
caw <christopherwood07@gmail.com>
2013-09-27 22:12:06 -0700

    Modified CKLabel algorithm to run for all trees...

--- src/trees/cklabel/CKLabel.java
+++ src/trees/cklabel/CKLabel.java
@@ -37,7 +37,7 @@ public class CKLabel
 				{
 					if (graph.subtreeHeight[u][v] != -1 && graph.subtreeHeight[u][v] == height)
 					{
-						System.out.println("analyzing subtree T(" + u + "," + v + ")");
+						//System.err.println("analyzing subtree T(" + u + "," + v + ")");
 						//if (u != 0) // we should NOT ignore this, even if it does touch the root
 						{
 							for (int a = 0; a <= majorLabel; a++)
@@ -51,7 +51,7 @@ public class CKLabel
 										
 										if (delta.delta[u][v][a][b] == true)
 										{
-											System.out.println("True for (" + a + ", " + b + ")");
+											//System.err.println("True for (" + a + ", " + b + ")");
 										}
 										
 										// Perform final check - we've reached the max height of the tree
@@ -86,7 +86,7 @@ public class CKLabel
 					//if (b != a && b != (a+1) && b != (a-1)) // we don't care about this guy since he's the root
 					{
 						// Check to see if this label pair satisfies the children
-						System.out.println("checking " + a + "," + b + " for root");
+						//System.err.println("checking " + a + "," + b + " for root");
 						if (delta.rootLabels(graph, a, b))
 						{
 							return true;
@@ -104,24 +104,23 @@ public class CKLabel
 	 * Main entry point of program
 	 * @param args
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 		// Check the command line arguments 		
-		if (args.length != 1)
+		if (args.length != 2)
 		{
-			System.err.println("usage: java CKLabel file");
+			System.err.println("usage: java CKLabel mode file");
 			return;
 		}
-        
-		// Try to create a buffer reader to parse the adjacency matrix
-		try
-		{
+		int mode = Integer.parseInt(args[0]);
+		if (mode == 0) // AMF file
+        	{
 			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            
+	    
 			// Now, read in the matrix dimensions
 			int dimensions = Integer.parseInt(reader.readLine());
 			int matrix[][] = new int[dimensions][dimensions];
-			
+		
 			// Continue parsing in the rest of the data
 			for (int i = 0; i < dimensions; i++)
 			{
@@ -132,35 +131,44 @@ public class CKLabel
 					matrix[i][j] = Integer.parseInt(elements[j]);
 				}
 			}
-		
+	
 			// Run the CK label algorithm here
 			CKLabel labeler = new CKLabel(new Graph(matrix, dimensions));
 			boolean success = labeler.runCK();
 			System.out.println(success);
-			
+		
 			// Now build the label scheme from this result
 			/*if (success)
 			{
-				
+			
 				HashMap<Integer, Integer> labelMap = new HashMap<Integer, Integer>();
 				labeler.delta.generateLabels(labelMap);
 				System.out.println(labelMap);
 			}*/
 		}
-		catch (IndexOutOfBoundsException ex1)
+		else if (mode == 1) //  file with G6 Strings
 		{
-			System.err.println("Error parsing adjacency matrix file.");
-			ex1.printStackTrace();
-		}
-		catch (NumberFormatException ex2)
-		{
-			System.err.println("Error parsing adjacency matrix file.");
-			ex2.printStackTrace();
-		}
-		catch (IOException ex3)
-		{
-			System.err.println("Error parsing adjacency matrix file.");
-			ex3.printStackTrace();
+			BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+			String line = reader.readLine();
+			do
+			{
+				// Build the adjacency matrix and determine the span
+				int[][] matrix = G6Parser.parseG6(line);
+				CKLabel labeler = new CKLabel(new Graph(matrix, matrix.length));
+				boolean success = labeler.runCK();
+
+				// Display the results...
+				System.err.println(line);
+				System.err.println(success);
+				System.out.println(line);
+				System.out.println(success);
+
+				// Advance to the next graph
+				line = reader.readLine();
+			} while (line != null && line.length() > 0);
+
+			// Close up shop
+			reader.close();
 		}
 	}
 }
