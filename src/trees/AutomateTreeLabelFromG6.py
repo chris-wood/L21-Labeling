5453f0350310d60a84f57f05fafc37b6a185fe98
chris-wood <christopherwood07@gmail.com>
2013-11-27 18:48:57 -0800

    Updated automate tree label thingy

--- src/trees/AutomateTreeLabelFromG6.py
+++ src/trees/AutomateTreeLabelFromG6.py
@@ -14,10 +14,11 @@ import glob
 
 print >> sys.stderr, len(sys.argv)
 if (len(sys.argv) != 3):
-	print "Usage: python AutomateLabel.py alg fileOfFiles"
+	print "Usage: python AutomateLabel.py alg fileOfFiles file?"
 else:
 	alg = sys.argv[1]
 	fof = sys.argv[2]
+	tofile = int(sys.argv[3])
 
 	fofn = open(fof, 'r')
 	for fname in fofn:
@@ -25,27 +26,34 @@ else:
 		if (len(fname) > 0):
 			# Obtain the results
 			print >> sys.stderr, "Running: " + fname
-			p = subprocess.Popen('java ' + alg + " 1 " + fname, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-			lines = []
-			for l in p.stdout.readlines():
-				l = l.strip()
-				lines.append(l) # the program will output two lines per graph: the g6 string and the span pass/fail flag
+			tempout = open(fname + ".out", 'w')
 
-			# Parse the results and put the files in the appropriate files
-			print >> sys.stderr, "Parsing results and dumping output of N files: " + str(len(lines) / 2)
-			d1 = []
-			d2 = []
-			for i in range(0, len(lines), 2):
-				if "true" in lines[i + 1]:
-					d1.append(lines[i])
-				else:
-					d2.append(lines[i])
+			if (tofile == 0):
+				p = subprocess.Popen('java ' + alg + " 1 " + fname, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+				lines = []
+				for l in p.stdout.readlines():
+					l = l.strip()
+					lines.append(l) # the program will output two lines per graph: the g6 string and the span pass/fail flag
 
-			# Write the results to the file
-			print >> sys.stderr, "Dump output to " + fname + "_d1.txt and " + fname + "_d2.txt"
-			d1f = open(fname + "_d1.txt", 'w')
-			for g in d1:
-				d1f.write(g + "\n")
-			d2f = open(fname + "_d2.txt", 'w')
-			for g in d2:
-				d2f.write(g + "\n")
+				# Parse the results and put the files in the appropriate files
+				print >> sys.stderr, "Parsing results and dumping output of N files: " + str(len(lines) / 2)
+				d1 = []
+				d2 = []
+				for i in range(0, len(lines), 2):
+					if "true" in lines[i + 1]:
+						d1.append(lines[i])
+					else:
+						d2.append(lines[i])
+
+				# Write the results to the file
+				print >> sys.stderr, "Dump output to " + fname + "_d1.txt and " + fname + "_d2.txt"
+				d1f = open(fname + "_d1.txt", 'w')
+				for g in d1:
+					d1f.write(g + "\n")
+				d2f = open(fname + "_d2.txt", 'w')
+				for g in d2:
+					d2f.write(g + "\n")
+			else: # to avoid memory error...
+				p = subprocess.Popen('java ' + alg + " 1 " + fname, shell=True, stdout=tempout, stderr=subprocess.STDOUT)
+				retcode = p.wait()
+				tempout.flush()
